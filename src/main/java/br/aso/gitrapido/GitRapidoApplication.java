c037a24055aee6d78be27f6f2a00e6cfc95352ee
Almir Oliveira <almir.so@gmail.com>
2021-04-05 21:00:54 -0300

    first commit

---
+++ src/main/java/br/aso/gitrapido/GitRapidoApplication.java
@@ -0,0 +1,13 @@
+package br.aso.gitrapido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRapidoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitRapidoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/aso/gitrapido/GitRapidoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.aso.gitrapido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRapidoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
